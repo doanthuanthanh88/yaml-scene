56f16d5c9a19badb045a95ba49950814c1f4d513
doanthuanthanh88 <doanthuanthanh88@gmail.com>
2022-03-09 09:11:38 +0000

    Update build function
    Copy documents in npm package before deploy

--- bin/update_package.js
+++ bin/update_package.js
@@ -1,6 +1,15 @@
-const { writeFileSync, readFileSync } = require('fs')
+const { writeFileSync, readFileSync, copyFileSync } = require('fs')
 const { join } = require('path')
-const packageFile = join(__dirname, '..', 'dist/package.json')
-let json = readFileSync(packageFile).toString()
-json = json.replace(/^\s*"prepare"\:.+\n/m, '')
-writeFileSync(packageFile, json)
+
+new Array('package.json', 'package-lock.json', 'yarn.lock', 'README.md', 'GUIDE.md')
+  .forEach(file => {
+    const fout = join(__dirname, '..', 'dist', file)
+    const fin = join(__dirname, '..', file)
+    if (file === 'package.json') {
+      let json = readFileSync(fin).toString()
+      json = json.replace(/^\s*"prepare"\:.+\n/m, '')
+      writeFileSync(fout, json)
+      return
+    }
+    copyFileSync(fin, fout)
+  })
