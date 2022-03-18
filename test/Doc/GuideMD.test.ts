88cfa2ebae9bebc8f0f90e7ebfccc3c2f24c5c98
Doan Thuan Thanh <doanthuanthanh88@gmail.com>
2022-03-18 16:17:48 +0000

    Update unit test for Doc/Guide/MD

--- test/Doc/GuideMD.test.ts
+++ test/Doc/GuideMD.test.ts
@@ -2,6 +2,20 @@ import { Simulator } from "@app/Simulator"
 import { readFileSync } from "fs"
 import { join } from "path"
 
+/**
+ * @guide
+ * @name Doc/Guide/MD
+ * @group Doc
+ * @description Auto scan file to detect the comment format which is generated to markdown document
+ * @example
+- Doc/Guide/MD: 
+    includes: 
+      - src
+    excludes: []
+    includePattern: ".+\\.ts$"
+    outFile: /tmp/doc.md
+ * @end
+ */
 describe('Test to generate guideline document', () => {
 
   test('Export to guideline document markdown', async () => {
